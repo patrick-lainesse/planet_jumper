f142844a593952ac83b6d9f7ab01a97b87bed44b
Rimousqui <patrick.lainesse@umontreal.ca>
2020-03-19 13:41:40 -0400

    Première tentative d'ajout d'un card view pour afficher les statistiques du vaisseau sélectionné.

--- app/src/main/java/com/example/planet_jumper/ChoixVaisseau.kt
+++ app/src/main/java/com/example/planet_jumper/ChoixVaisseau.kt
@@ -1,13 +1,17 @@
 package com.example.planet_jumper
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class ChoixVaisseau: AppCompatActivity(), View.OnClickListener {
 
+    // utilisation d'un ArrayList pour faciliter les manipulations répétitives sur les ImageView
     lateinit var vaisseau1: ImageView
     lateinit var vaisseau2: ImageView
     lateinit var vaisseau3: ImageView
@@ -42,14 +46,27 @@ class ChoixVaisseau: AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
 
+        val layout = LinearLayout(v?.context)
+        val layoutInflater = LayoutInflater.from(this)
+
         if (v != null) {
             when(v.id) {
                 R.id.vaisseau1 -> Toast.makeText(this, "v1", Toast.LENGTH_LONG).show()
                 R.id.vaisseau2 -> Toast.makeText(this, "v2", Toast.LENGTH_LONG).show()
                 R.id.vaisseau3 -> Toast.makeText(this, "v3", Toast.LENGTH_LONG).show()
                 R.id.vaisseau4 -> Toast.makeText(this, "v4", Toast.LENGTH_LONG).show()
-
             }
+
+            for (objet in table_vaisseau) {
+                objet.visibility = View.GONE
+            }
+
+            //layout.addView(cardView)
+            val card = layoutInflater.inflate(R.layout.card_vaisseau, null)
+            layout.addView(card)
+
+
+
         }
     }
 }
