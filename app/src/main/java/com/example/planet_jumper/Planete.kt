862e765e71895bd1ef4744e7c7920adfe78908b7
Rimousqui <patrick.lainesse@umontreal.ca>
2020-04-16 15:33:50 -0400

    Ajout d'une fonctionnalité pour cacher ou montrer les fragments dans l'activité Planete pour éviter d'avoir à effectuer une nouvelle requête avec Volley chaque fois que le joueur veut visionner ses cartes.

--- app/src/main/java/com/example/planet_jumper/Planete.kt
+++ app/src/main/java/com/example/planet_jumper/Planete.kt
@@ -17,10 +17,12 @@ class Planete: AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.planete)
 
+        // récupère le choix de vaisseau qui a été fait afin de sélectionner la liste de cartes associées à ce vaisseau en particulier
         vaisseau = intent.extras!!.getString("choix").toString()
         setListener()
     }
 
+    // abonne les boutons à la fonction onclick
     private fun setListener() {
 
         val boutonCarte = findViewById<AppCompatButton>(R.id.bouton_cartes)
@@ -31,30 +33,60 @@ class Planete: AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
 
+        val CARTES_FRAG_TAG = "fragCartes"
+        val PLANETES_FRAG_TAG = "fragPlanetes"
+
+        /* identifie le bouton qui a été cliqué et crée, montre ou cache le fragment désiré selon la sélection
+        Puisque faire une requête au serveur nécessite un certain temps, il est préférable de cacher le fragment que de le détruire,
+        ce qui impliquerait une nouvelle requête à chaque fois */
         when (v?.id) {
             R.id.bouton_cartes -> {
-
-                // vérifie si le fragment pour afficher la liste des cartes existe déjà, et si non, le créer
-                if(supportFragmentManager.findFragmentByTag("fragCartes") == null) {
-
+                if(supportFragmentManager.findFragmentByTag(CARTES_FRAG_TAG) != null) {
+                    // si le fragment existe déjà, le montrer
+                    supportFragmentManager
+                        .beginTransaction()
+                        .show(supportFragmentManager.findFragmentByTag(CARTES_FRAG_TAG)!!)
+                        .commit()
+                } else {
+                    // s'il n'existe pas, le créer
                     val fragCartes: FragCartes = FragCartes.newInstance()
                     supportFragmentManager
                         .beginTransaction()
-                        .add(R.id.frame, fragCartes, "fragCartes")
+                        .add(R.id.frame, fragCartes, CARTES_FRAG_TAG)
                         .addToBackStack(null)
+                        .commit()
+                }
+                if(supportFragmentManager.findFragmentByTag(PLANETES_FRAG_TAG) != null) {
+                    // si l'autre fragment est visible, le cacher
+                    supportFragmentManager
+                        .beginTransaction()
+                        .hide(supportFragmentManager.findFragmentByTag(PLANETES_FRAG_TAG)!!)
                         .commit()
                 }
             }
             R.id.bouton_planetes -> {
 
-                // vérifie si le fragment pour afficher la liste des planètes existe déjà, et si non, le créer
-                if(supportFragmentManager.findFragmentByTag("fragPlanetes") == null) {
-
-                    val fragPlanetes: FragListePlanetes= FragListePlanetes.newInstance()
+                if(supportFragmentManager.findFragmentByTag(PLANETES_FRAG_TAG) != null) {
+                    // si le fragment existe déjà, le montrer
                     supportFragmentManager
                         .beginTransaction()
-                        .add(R.id.frame, fragPlanetes, "fragPlanetes")
+                        .show(supportFragmentManager.findFragmentByTag(PLANETES_FRAG_TAG)!!)
+                        .commit()
+                } else {
+                    // s'il n'existe pas, le créer
+
+                    val fragPlanetes: FragListePlanetes = FragListePlanetes.newInstance()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .add(R.id.frame, fragPlanetes, PLANETES_FRAG_TAG)
                         .addToBackStack(null)
+                        .commit()
+                }
+                if(supportFragmentManager.findFragmentByTag(CARTES_FRAG_TAG) != null) {
+                    // si l'autre fragment est visible, le cacher
+                    supportFragmentManager
+                        .beginTransaction()
+                        .hide(supportFragmentManager.findFragmentByTag(CARTES_FRAG_TAG)!!)
                         .commit()
                 }
             }
